b0b7f59ba5fcdcd62f02433c5db9a2f4c7d22c04
FrazaoNeto23 <joao.b.neto34@aluno.senai.br>
2025-11-07 16:56:36 -0300

    .

--- veiculo_formulario.php
+++ veiculo_formulario.php
@@ -5,35 +5,35 @@
 <label>Tipo combustivel</label>
 <select>
     <option value="">Selecione o Tipo</option>
-    <option value="1"Diesel></option>
-    <option value="2"Flex></option>
-    <option value="3"Etanol></option>
-    <option value="4"Gasolina></option>
+    <option value="1" Diesel></option>
+    <option value="2" Flex></option>
+    <option value="3" Etanol></option>
+    <option value="4" Gasolina></option>
 </select>
 
 <label>Placa</label>
-<input type="text" placeholder="Escreva a sua placa"/>
+<input type="text" placeholder="Escreva a sua placa" />
 
 <label>Ano</label>
-<input type="text" placeholder="Escreva o ano"/>
+<input type="text" placeholder="Escreva o ano" />
 
 <label>Cor</label>
-<input type="text" placeholder="Escreva a cor"/>
+<input type="text" placeholder="Escreva a cor" />
 
 <select>
     <option value="">Selecione o Status</option>
-    <option value="1"Disponivel></option>
-    <option value="2"Em Rota></option>
-    <option value="3"Manutenção></option>
+    <option value="1" Disponivel></option>
+    <option value="2" Em Rota></option>
+    <option value="3" Manutenção></option>
 </select>
 
 <label>Km Inicial</label>
-<input type="text" placeholder="Escreva o km"/>
+<input type="text" placeholder="Escreva o km" />
 
 <label>Data Última Manutenção</label>
-<input type="date" placeholder="Escreva a data"/>
+<input type="datetime-local" placeholder="Escreva a data" />
 
 <label>Intervalo de Manutenção</label>
-<input type="text" placeholder="Escreva a cor"/>
+<input type="text" placeholder="Escreva em dias" />
 
 <?php include("includes/footer.php"); ?>
